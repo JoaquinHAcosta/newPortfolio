036dcfb9eb6b00a10ed4e57febe1ceef0bbc9359
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-09-24 19:23:28 -0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className='desktop-nav'>
+            <div className='logo'>Joaquin Acosta</div>
+            <div>
+                <ul className='nav-links'>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
